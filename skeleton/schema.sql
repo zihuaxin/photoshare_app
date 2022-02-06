a6899df3f47e4a337cedce3058b6977c61bd29f0
Sarsen Whatmore <68308751+revelationsz@users.noreply.github.com>
2022-02-06 16:00:49 -0500

    Update schema.sql
    Added in all tables we need and tested cross table compatability

--- skeleton/schema.sql
+++ skeleton/schema.sql
@@ -2,10 +2,21 @@ CREATE DATABASE IF NOT EXISTS photoshare;
 USE photoshare;
 DROP TABLE IF EXISTS Pictures CASCADE;
 DROP TABLE IF EXISTS Users CASCADE;
+DROP TABLE IF EXISTS Albums CASCADE;
+DROP TABLE IF EXISTS Photos CASCADE;
+DROP TABLE IF EXISTS Tags CASCADE;
+DROP TABLE IF EXISTS Comments CASCADE;
+
+
 
 CREATE TABLE Users (
     user_id int4  AUTO_INCREMENT,
     email varchar(255) UNIQUE,
+    first_name varchar(255) UNIQUE,
+    last_name varchar(255) UNIQUE,
+    dob varchar(255) UNIQUE,
+    hometown varchar(255) UNIQUE,
+    gender varchar(255) UNIQUE,
     password varchar(255),
   CONSTRAINT users_pk PRIMARY KEY (user_id)
 );
@@ -19,5 +30,39 @@ CREATE TABLE Pictures
   INDEX upid_idx (user_id),
   CONSTRAINT pictures_pk PRIMARY KEY (picture_id)
 );
-INSERT INTO Users (email, password) VALUES ('test@bu.edu', 'test');
+
+CREATE TABLE Albums (
+	album_id int4 AUTO_INCREMENT,
+    album_name varchar(255),
+    date_of_creation varchar(255),
+    creator_id int4,
+    CONSTRAINT albums_pk PRIMARY KEY (album_id)
+);
+
+CREATE TABLE Photos(
+	photo_id int4 AUTO_INCREMENT,
+    alblum_id int4,
+    caption varchar(255),
+	imgdata longblob, 
+	 CONSTRAINT photos_pk PRIMARY KEY (photo_id)
+);
+
+CREATE TABLE Tags(
+	tag_id int4 AUTO_INCREMENT,
+	tag varchar(255),
+-- how to make is so each photo has mutiple tags (*array of tag id's IDK*)
+    CONSTRAINT tags_pk PRIMARY KEY (tag_id)
+);
+
+CREATE TABLE Comments(
+	comment_id int4 AUTO_INCREMENT,
+    text varchar(255),
+    owner_id int4,
+    date_of_comment varchar(255),
+     CONSTRAINT comments_pk PRIMARY KEY (comment_id)
+);
+
+INSERT INTO Users (email, first_name, last_name, dob, hometown, gender, password) VALUES ('test@bu.edu', 'sarsen', 'whatmore', '2/2/2002', 'home', 'm', 'test');
 INSERT INTO Users (email, password) VALUES ('test1@bu.edu', 'test');
+INSERT INTO Albums (album_name, date_of_creation, creator_id) VALUES ("first", "2/2/2022", (SELECT user_id FROM photoshare.users WHERE first_name = "sarsen") );
+SELECT * FROM photoshare.users ;
