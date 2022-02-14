fa6e71a128ad075086ca42a53b92cf5a224330ab
@zihuaxin <zihuaxin@bu.edu>
2022-02-13 21:40:10 -0500

    updated schema and ER diagram

--- skeleton/schema.sql
+++ skeleton/schema.sql
@@ -1,26 +1,41 @@
 CREATE DATABASE IF NOT EXISTS photoshare;
+
 USE photoshare;
-DROP TABLE IF EXISTS Pictures CASCADE;
+
+DROP TABLE IF EXISTS album_user CASCADE;
+DROP TABLE IF EXISTS photo_tags CASCADE;
+DROP TABLE IF EXISTS user_comment CASCADE;
+DROP TABLE IF EXISTS album_photo CASCADE;
 DROP TABLE IF EXISTS Users CASCADE;
+DROP TABLE IF EXISTS Friends CASCADE;
 DROP TABLE IF EXISTS Albums CASCADE;
-DROP TABLE IF EXISTS Photos CASCADE;
 DROP TABLE IF EXISTS Tags CASCADE;
 DROP TABLE IF EXISTS Comments CASCADE;
 
-
+# ENTITIES
 
 CREATE TABLE Users (
     user_id int4  AUTO_INCREMENT,
     email varchar(255) UNIQUE,
-    first_name varchar(255) UNIQUE,
-    last_name varchar(255) UNIQUE,
-    dob varchar(255) UNIQUE,
-    hometown varchar(255) UNIQUE,
-    gender varchar(255) UNIQUE,
-    password varchar(255),
-  CONSTRAINT users_pk PRIMARY KEY (user_id)
+    first_name varchar(255),
+    last_name varchar(255),
+    dob DATE, # probably better as DATE
+    hometown varchar(255),
+    gender INTEGER, # 0: male, 1: female, 2: others?
+    password varchar(255) UNIQUE,
+	CONSTRAINT users_pk PRIMARY KEY (user_id)
 );
 
+# symmetrical friendship
+CREATE TABLE Friends (
+     user_id1 INT NOT NULL REFERENCES Users(user_id),
+     user_id2 INT NOT NULL REFERENCES Users(user_id),
+     CONSTRAINT PK_Friends_user_id1_user_id2 PRIMARY KEY (user_id1, user_id2),
+     CONSTRAINT UQ_Friends_user_id1_user_id2 UNIQUE (user_id2, user_id1)
+);
+
+# not sure if this is duplicate of Photos
+/*
 CREATE TABLE Pictures
 (
   picture_id int4  AUTO_INCREMENT,
@@ -30,39 +45,75 @@ CREATE TABLE Pictures
   INDEX upid_idx (user_id),
   CONSTRAINT pictures_pk PRIMARY KEY (picture_id)
 );
+*/
 
 CREATE TABLE Albums (
 	album_id int4 AUTO_INCREMENT,
     album_name varchar(255),
-    date_of_creation varchar(255),
-    creator_id int4,
+    date_of_creation DATE,
     CONSTRAINT albums_pk PRIMARY KEY (album_id)
 );
 
-CREATE TABLE Photos(
+CREATE TABLE album_photo (
 	photo_id int4 AUTO_INCREMENT,
-    alblum_id int4,
     caption varchar(255),
 	imgdata longblob, 
-	 CONSTRAINT photos_pk PRIMARY KEY (photo_id)
+    album_id int4  NOT NULL,
+    PRIMARY KEY (photo_id, album_id),
+    FOREIGN KEY (album_id) REFERENCES Albums(album_id) ON DELETE CASCADE
 );
 
 CREATE TABLE Tags(
 	tag_id int4 AUTO_INCREMENT,
-	tag varchar(255),
--- how to make is so each photo has mutiple tags (*array of tag id's IDK*)
+    
+    # NOTE: lower case constraint needs to be added during insertion
+	tag_name varchar(255) UNIQUE,
+    
     CONSTRAINT tags_pk PRIMARY KEY (tag_id)
 );
 
 CREATE TABLE Comments(
 	comment_id int4 AUTO_INCREMENT,
     text varchar(255),
-    owner_id int4,
-    date_of_comment varchar(255),
+    date_of_comment DATE,
      CONSTRAINT comments_pk PRIMARY KEY (comment_id)
 );
 
-INSERT INTO Users (email, first_name, last_name, dob, hometown, gender, password) VALUES ('test@bu.edu', 'sarsen', 'whatmore', '2/2/2002', 'home', 'm', 'test');
-INSERT INTO Users (email, password) VALUES ('test1@bu.edu', 'test');
-INSERT INTO Albums (album_name, date_of_creation, creator_id) VALUES ("first", "2/2/2022", (SELECT user_id FROM photoshare.users WHERE first_name = "sarsen") );
-SELECT * FROM photoshare.users ;
+# RELATIONSHIPS
+
+# one to many
+# participation constraint?
+CREATE TABLE album_user (
+	user_id int4,
+    album_id int4,
+    PRIMARY KEY (album_id),
+    FOREIGN KEY (album_id) REFERENCES Albums(album_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+# many to many
+# no participation constraints
+CREATE TABLE photo_tags (
+	photo_id int4,
+    tag_id int4,
+    PRIMARY KEY (photo_id, tag_id),
+    FOREIGN KEY (photo_id) REFERENCES album_photo(photo_id),
+    FOREIGN KEY (tag_id) REFERENCES Tags(tag_id)
+);
+
+# one to many
+# no participation constraints? (user can be banned but their comments can be left behind )
+CREATE TABLE user_comment(
+	user_id int4,
+    comment_id int4,
+    PRIMARY KEY (comment_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (comment_id) REFERENCES Comments(comment_id)
+);
+
+INSERT INTO Users (email, first_name, last_name, dob, hometown, gender, password) VALUES ('test@bu.edu', 'sarsen', 'whatmore', '1997-06-10', 'home', 0, 'test0');
+INSERT INTO Users (email, password) VALUES ('test1@bu.edu', 'test1');
+INSERT INTO Albums (album_name, date_of_creation) VALUES ("first", '1997-06-10');
+INSERT INTO Tags (tag_name) VALUES ("Boston");
+
+SELECT tag_name FROM photoshare.tags ;
